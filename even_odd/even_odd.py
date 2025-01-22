059b27ca0c343704eb3a5097eecdafdb994e87d0
Ralph Cajipe <ralphcajipe@gmail.com>
2025-01-22 10:38:23 +0800

    Implement even and odd number counting logic

--- even_odd/even_odd.py
+++ even_odd/even_odd.py
@@ -3,7 +3,11 @@ numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 even_count = 0
 odd_count = 0
     
-ex
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
         
 print(f"Number of even numbers: {even_count}")
 print(f"Number of odd numbers: {odd_count}")
